35a307ebe528832e68e5eb5bf6a36610a5a98c58
003falcon <333mohammedmohtasim@gmail.com>
2025-05-05 14:46:09 +0530

    some minute changes to earlier projects

--- project-2/practice/Contact.jsx
+++ project-2/practice/Contact.jsx
@@ -3,31 +3,23 @@
 //without destructuring
 // export default function Contact(props)
 //with destructuring
-export default function Contact({num,img_name ,title,ph_no,mail_id})
-
-{
+export default function Contact({ num, img_name, title, ph_no, mail_id }) {
   // console.log(props)
-  return <article className="contact-card">
-    
-  <img 
-      src={img_name}
-      alt="Photo of {props.title}"
-  />
-  <h3>{num}{title}</h3>
-  <div className="info-group">
-      <img 
-          src="./imgs/phone-icon.png" 
-          alt="phone icon" 
-      />
-      <p>{ph_no}</p>
-  </div>
-  <div className="info-group">
-      <img 
-          src="./imgs/mail-icon.png" 
-          alt="mail icon"
-      />
-      <p>{mail_id}</p>
-  </div>
-</article>
-
+  return (
+    <article className="contact-card">
+      <img src={img_name} alt={`Photo of ${title}`} />
+      <h3>
+        {num}
+        {title}
+      </h3>
+      <div className="info-group">
+        <img src="./imgs/phone-icon.png" alt="phone icon" />
+        <p>{ph_no}</p>
+      </div>
+      <div className="info-group">
+        <img src="./imgs/mail-icon.png" alt="mail icon" />
+        <p>{mail_id}</p>
+      </div>
+    </article>
+  );
 }
