73754e69b981c421856201cf25f7860dfe87c503
003falcon <333mohammedmohtasim@gmail.com>
2025-07-07 23:00:38 +0530

    conditional rendering used in chef claude

--- project-3/Header.jsx
+++ project-3/Header.jsx
@@ -1,7 +1,8 @@
-export default function Header()
-{
-  return <header>
-  <img src="./image.png" alt="chef_hat_image"/>
-  <h1>Chef Claude</h1>;
-  </header>
+export default function Header() {
+  return (
+    <header>
+      <img src="./image.png" alt="chef_hat_image" />
+      <h1>Chef Claude</h1>
+    </header>
+  );
 }
--- project-3/Main.jsx
+++ project-3/Main.jsx
@@ -11,32 +11,33 @@ export default function Main() {
   //   event.preventDefault();
   //   const formData = new FormData(event.currentTarget);
   //   const newIngredient = formData.get("ingredient");
-    //after
-    // setCurrentIngredients((prevIngredients) => [
-    //   ...prevIngredients,
-    //   newIngredient,
-    // ]);
-    // console.log(newIngredient);
-    //before
-    // ingredients.push(newIngredient)
-    // console.log(ingredients);
-    // console.log("Form submitted");
+  //after
+  // setCurrentIngredients((prevIngredients) => [
+  //   ...prevIngredients,
+  //   newIngredient,
+  // ]);
+  // console.log(newIngredient);
+  //before
+  // ingredients.push(newIngredient)
+  // console.log(ingredients);
+  // console.log("Form submitted");
 
-    //Props vs State
+  //Props vs State
 
-    //props are immutable with its values not changed in the component
-    //states are mutable within the component and are used when temp/local variables used in the component are
-    //to be saved or displayed
+  //props are immutable with its values not changed in the component
+  //states are mutable within the component and are used when temp/local variables used in the component are
+  //to be saved or displayed
   // }
 
-  function addIngredient(formData){
-    const newIngredient =formData.get('ingredient');
+  function addIngredient(formData) {
+    const newIngredient = formData.get("ingredient");
     console.log(newIngredient);
-    setCurrentIngredients((prevIngredients) => [
+    if (newIngredient != "") {
+      setCurrentIngredients((prevIngredients) => [
         ...prevIngredients,
         newIngredient,
       ]);
-
+    } else alert("Enter ingredient :)");
   }
   return (
     <main>
@@ -44,13 +45,30 @@ export default function Main() {
       <form action={addIngredient} className="add-ingredient-form">
         <input
           type="text"
-          placeholder="e.g. mutton biryani"
+          placeholder="e.g. chili powder"
           aria-label="Add ingredient"
           name="ingredient"
         />
         <button>Add ingredient</button>
       </form>
-      <ul>{ingredientsItems}</ul>
+      {/* <ul>{ingredientsItems}</ul> */}
+      {currentIngredients.length ? (
+        <section>
+          <h2>Ingredients on hand:</h2>
+          <ul className="ingredients-list" aria-live="polite">
+            {ingredientsItems}
+          </ul>
+          {currentIngredients.length > 4 && (
+            <div className="get-recipe-container">
+              <div>
+                <h3>Ready for a recipe?</h3>
+                <p>Generate a recipe from your list of ingredients.</p>
+              </div>
+              <button>Get a recipe</button>
+            </div>
+          )}
+        </section>
+      ) : null}
     </main>
   );
 }
