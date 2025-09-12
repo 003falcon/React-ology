124b7c15c019b37ec65796160e087ffd6d695405
003falcon <333mohammedmohtasim@gmail.com>
2025-09-12 23:20:10 +0530

    subtle changes done  to add hints

--- Tenzies/App.jsx
+++ Tenzies/App.jsx
@@ -2,25 +2,24 @@ import Dice from "./Dice";
 import { useState } from "react";
 // import React from 'react'
 
-import Conf from './Conf'
+import Conf from "./Conf";
 
 export default function App() {
-
   const [diceArr, setDiceArr] = useState(allNewDice());
-
+  const num = 3;
+  let display = "";
   let gameWon = false;
   if (
     diceArr.every((die) => die.isHeld) &&
     diceArr.every((die) => die.value == diceArr[0].value)
-  )
-  {
-    gameWon=true;
-    console.log("Game Won!");
+  ) {
+    gameWon = true;
+    display = diceArr[0].value == num ? "Congratulations! You have won the game,Here's the codename to the next hint: EF5" : "Oops ,Try Again :(";
+    // console.log("Game Won!");
   }
 
   const buttonText = gameWon ? "New Game" : "Roll";
-  function resetDice()
-  {
+  function resetDice() {
     setDiceArr(allNewDice());
   }
   function allNewDice() {
@@ -76,11 +75,13 @@ export default function App() {
 
   return (
     <main>
-    {
-      gameWon && <Conf/>
-    }
+      {gameWon && <Conf />}
       <h1 className="title">Tenzies</h1>
-    <p className="game-rules"> Roll until all the dice are the same. Click each die to freeze it at its current value between rolls.</p>
+      <p className="game-rules">
+        {" "}
+        Roll until all the dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
       <div className="dice-container">
         {/* <Dice value={1}/>
     <Dice value={2}/>
@@ -94,10 +95,14 @@ export default function App() {
     <Dice value={4}/> */}
         {diceElem}
       </div>
-      <button className="roll-button" onClick={gameWon ? resetDice: rollAllDice }>
+      <button
+        className="roll-button"
+        onClick={gameWon ? resetDice : rollAllDice}
+      >
         {buttonText}
       </button>
-    {/* {gameWon && alert("Congratulations! You have won the game!!!")} */}
+      {gameWon &&
+        alert(display)}
     </main>
   );
 }
