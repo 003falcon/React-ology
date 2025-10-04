bbfb3b8cc213fd0495b61a404e13b36309e41e33
003falcon <333mohammedmohtasim@gmail.com>
2025-10-04 13:09:38 +0530

    build header and status section of Jus'GuessIt game

---
+++ JusGuessIt/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <main>
+      <header>
+      <h1>
+        Jus' Guess It!
+        </h1>
+        <p>
+      Guess the right word within 8 attempts to save the world from assembly taking over!
+        </p>
+        </header>
+      <div className="game-status">
+        <h2>You win!</h2>
+        <h4>Well done!🎉</h4>
+      </div>
+      
+    </main>
+  );
+}
