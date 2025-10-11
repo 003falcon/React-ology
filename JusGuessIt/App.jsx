58b4c8dd7543963f601a161fc60e7029ece53e4c
003falcon <333mohammedmohtasim@gmail.com>
2025-10-11 23:25:33 +0530

    Add languages list

--- JusGuessIt/App.jsx
+++ JusGuessIt/App.jsx
@@ -1,6 +1,11 @@
 import React from "react";
-
+import {languages} from './languages.js'
+console.log(languages);
 export default function App() {
+
+  const langElements=languages.map((langObj)=>
+    <span className="elem" style={{backgroundColor:langObj.backgroundColor,color:langObj.color}}>{langObj.name}</span>
+)
   return (
     <main>
       <header>
@@ -8,13 +13,17 @@ export default function App() {
         Jus' Guess It!
         </h1>
         <p>
-      Guess the right word within 8 attempts to save the world from assembly taking over!
+      Guess the right word within 8 attempts to save the world from  taking over!
         </p>
         </header>
+      
       <div className="game-status">
         <h2>You win!</h2>
         <h4>Well done!🎉</h4>
       </div>
+      <section className="game-lang">
+    {langElements}
+      </section>
       
     </main>
   );
---
+++ JusGuessIt/languages.js
@@ -0,0 +1,44 @@
+export const languages=[
+  {
+    name: "English",
+    backgroundColor: "#ffffff",
+    color: "#000000",    
+},
+{
+  name: "Spanish",
+  backgroundColor: "#000000",
+  color: "#ffffff",
+
+},
+{
+  name: "Arabic",
+  backgroundColor: "#0000ff",
+  color: "#ffff00",
+},
+{
+  name: "German",
+  backgroundColor: "#ff0000", 
+  color: "#00ff00",
+},
+{ name: "Hindi",    
+  backgroundColor: "#00ff00",
+  color: "#ff0000",
+},
+{ name: "Urdu",
+  backgroundColor: "#ffff00",
+  color: "#0000ff",
+},
+{ name: "Dutch",  
+  backgroundColor: "#ffa500",
+  color: "#800080",
+},
+{ name: "Russian",  
+  backgroundColor: "#800080",
+  color: "#ffa500",
+},
+{ name: "Telugu",  
+  backgroundColor: "#008000",       
+  color: "#ff69b4",
+},
+
+];
